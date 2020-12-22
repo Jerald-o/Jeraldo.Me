d3b743f001e5e65b09fb5c27a56f6fab1a2fc4ff
Dylan Edwards <dylanedwards290@gmail.com>
2020-12-21 19:32:58 -0800

    add Styled Header and spacing component with props

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import InstagramIcon from "../icons/InstagramIcon";
 
 const NavStyles = styled.nav`
   height: 70px;
@@ -15,23 +16,53 @@ const NavStyles = styled.nav`
   }
 `;
 
+const SocialLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px;
+`;
+
+const SocialItem = styled.a`
+  padding: 10px;
+`;
+
+const Spacing = styled.div`
+  padding: ${props => props.spacingAmount ? props.spacingAmount : "15px"};
+  background-color: ${props => props.color ? props.color : "red"};
+`;
+
+
+
 export default function Nav() {
   return (
     <NavStyles>
-      <ul>
-        <li>
-          <Link to="/">Home </Link>
-        </li>
-        <li>
-          <Link to="/content">Content</Link>
-        </li>
-        <li>
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-      </ul>
+      <SocialLinks>
+        <SocialItem>
+          <InstagramIcon />
+        </SocialItem>
+      </SocialLinks>
+      <Spacing color={"purple"} spacingAmount={"1px"}/>
     </NavStyles>
   );
+}
+
+{
+  /* <ul>
+<li>
+  <a href=""></a>
+</li>
+<li>
+  <Link to="/">Home</Link>
+</li>
+<li>
+  <Link to="/content">Content</Link>
+</li>
+<li>
+  <Link to="/portfolio">Portfolio</Link>
+</li>
+<li>
+  <Link to="/about">About</Link>
+</li>
+</ul> */
 }
